7a1517be094229974534408a2544ea177b06011c
James T Ardian <jamesardian01@gmail.com>
2022-07-31 00:35:20 -0700

    minor change in UI styling for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import { Flex, Box, Heading, Spacer } from '@chakra-ui/react'
 
 const Header = () => {
   return (
-    <Flex minWidth='max-content' alignItems='center' gap='2' color='lightGrey' bgColor='black'>
-      <Box px='8' py='4'>
+    <Flex minWidth='max-content' alignItems='center' gap='1em' color='lightGrey' bgColor='black'>
+      <Box px='2em' py='1em'>
         <Heading size='lg'>Go Chat</Heading>
       </Box>
       <Spacer />
